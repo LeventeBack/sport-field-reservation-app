92254e8893b3f0092661e2d8f51cb235a977a1c4
LeventeBack <backistvanlevente17@gmail.com>
2025-10-01 17:32:27 +0300

    update lint settings

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,35 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+  env: { browser: true, es2020: true, es6: true },
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "react-refresh",
+    "unused-imports",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    "react/react-in-jsx-scope": "off",
+    "unused-imports/no-unused-imports": "warn",
   },
-}
+};
